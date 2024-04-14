50d74ca59f1aab772c572468dd213bc8d6c6f238
Shubham Shinde <shubhamshinde6762@gmail.com>
2024-04-14 23:01:39 +0530

    frontent deploy

--- main/src/components/Login.js
+++ main/src/components/Login.js
@@ -21,7 +21,7 @@ const Login = ({ socket, currentUser, setCurrentUser }) => {
 
   return (
     <motion.div
-      className="w-full flex justify-center items-center h-[92vh] overflow-clip bg-gradient-to-b from-orange-200 via-sky-100 to-green-200"
+      className="w-full flex sx:flex-wrap xs:flex-wrap justify-center items-center h-[92vh] overflow-clip bg-gradient-to-b from-orange-200 via-sky-100 to-green-200"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 1, y: -100 }}
@@ -34,13 +34,13 @@ const Login = ({ socket, currentUser, setCurrentUser }) => {
           alt="background"
         />
       </div> */}
-      <div className="m-5 ">
+      <div className="m-5 w-[40%] sx:w-full">
         <motion.img
           initial={{ y: 100, opacity: 0, rotate: 30, x: -100 }}
           animate={{ y: 0, opacity: 1, rotate: 0, x: 0 }}
           exit={{ opacity: 0, rotate: -90, x: -500 }}
           transition={{ delay: 0.1, duration:0.5}}
-          className="min-w-[200px]"
+          className="min-w-[170px] sx:w-full w-full max-w-[600px] "
           src="https://res.cloudinary.com/dd6sontgf/image/upload/v1713013132/pngwing.com_jvwy32.png"
         />
       </div>
